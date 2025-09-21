170e6ec9080af76141f7c875eab2826c470a569a
estebansar <sar17008@byui.edu>
2025-09-21 02:39:09 -0600

    ...finishing part 1.....

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import { getParkData } from "./parkService.mjs";
 
+window.addEventListener("DOMContentLoaded", () => {
+
 const parkData = getParkData();
+
+document.title = parkData.fullName;
+
+const disclaimerLink = document.querySelector(".disclaimer > a");
+if (disclaimerLink) {
+    disclaimerLink.href = parkData.url;
+    disclaimerLink.textContent = parkData.fullName;
+}
+
+const heroImg = document.querySelector("#park-header .hero-banner > img");
+const img0 = parkData?.images?.[0];
+if (heroImg && img0) {
+    heroImg.src = img0.url;
+    heroImg.alt = img0.altText || parkData.fullName;
+}
+
+const titleLink = document.querySelector(".hero-banner__title");
+  const subtitle = document.querySelector(".hero-banner__subtitle");
+  if (titleLink) titleLink.textContent = parkData.fullName;
+  if (subtitle) {
+    const [desig, states] = subtitle.querySelectorAll("span");
+    if (desig) desig.textContent = parkData.designation;
+    if (states) states.textContent = parkData.states;
+  }
+});
+
