62278c85ee84af303ea85e93ffe1745d00a632af
estebansar <sar17008@byui.edu>
2025-11-29 20:39:21 -0700

    creating visitor-center. js

---
+++ src/js/visitor-center.js
@@ -0,0 +1,5 @@
+import "../css/style.css";             
+import "../css/visitor-center.css";    
+
+import { getParkData, getParkVisitorCenterDetails } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
