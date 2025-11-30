97bd774fdbf3a58f176b316a05a3043bee7f13ad
estebansar <sar17008@byui.edu>
2025-11-29 20:42:00 -0700

    getting addresses

--- src/js/visitor-center.js
+++ src/js/visitor-center.js
@@ -6,3 +6,13 @@ import setHeaderFooter from "./setHeaderFooter.mjs";
 
 import { listTemplate, vcImageTemplate, vcAmenityTemplate } from "./templates.mjs";
 
+function getParam(param) {
+  const params = new URLSearchParams(location.search);
+  return params.get(param);
+}
+
+function splitAddresses(addresses = []) {
+  const physical = addresses.find(a => a.type === "Physical");
+  const mailing = addresses.find(a => a.type === "Mailing");
+  return { physical, mailing };
+}
